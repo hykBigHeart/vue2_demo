747a5a79d22f7f2711480c0abe9c6078c7334ed9
huyikun <3069479901@qq.com>
2023-02-16 09:48:53 +0800

    dashboard 仪表板页

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+<!-- Dashboard 父页面 -->
+  <div class="dashboard-container">
+    <!-- component 内置组件， is 决定渲染引入的哪一个自定义组件 -->
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+import editorDashboard from './editor'
+
+export default {
+  name: 'Dashboard',
+  components: { adminDashboard, editorDashboard },
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
+    }
+  }
+}
+</script>
